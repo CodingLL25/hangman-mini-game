25219d8a5fbf3d1c1b3ffda850f578e7b523b37b
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-08-19 18:04:24 +0100

    Add game instructions to guide user

--- run.py
+++ run.py
@@ -10,3 +10,25 @@ print(
 ===============================================================================
 """
 )
+
+
+def game_instructions():
+    """Detailed instructions for user on how to play the game"""
+    print(
+        """
+    1. A random word has been generated, you'll need to guess the word one
+    letter at a time, before you run out of lives.\n
+    2. For each incorrect answer, parts of your hangman will appear.\n
+    3. When the hangman is complete, you have lost the game! Guess the word
+    correctly before your hangman has built, and you've won the game!\n
+
+    Time to start playing...
+    """
+    )
+
+
+def main():
+    game_instructions()
+
+
+main()
